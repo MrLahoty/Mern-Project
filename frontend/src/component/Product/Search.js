778e321266e5cd6dd8ed8a17a59383e590d2f846
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2023-06-26 17:40:51 +0530

    first commit

---
+++ frontend/src/component/Product/Search.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import MetaData from "../layout/MetaData";
+import "./Search.css";
+
+const Search = () => {
+  const [keyword, setKeyword] = useState("");
+  const history = useHistory();
+
+  const searchSubmitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/products/${keyword}`);
+    } else {
+      history.push("/products");
+    }
+  };
+
+  return (
+    <>
+      <MetaData title="Search A Product -- ECOMMERCE" />
+      <form className="searchBox" onSubmit={searchSubmitHandler}>
+        <input
+          type="text"
+          placeholder="Search a Product ..."
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </>
+  );
+};
+
+export default Search;
